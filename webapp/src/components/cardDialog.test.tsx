c679d9b4640f5384231f9d8032b936a98a24ac70
root <root@ip-172-31-38-224.ap-southeast-7.compute.internal>
2026-02-26 22:03:38 +0700

    fix(tests): polyfill DOMRect for jsdom and update snapshots
    - Add DOMRect polyfill in cardDialog.test.tsx since jsdom doesn't provide it
    - Update cardDialog and cardDetail snapshots to match new sticky header markup

--- webapp/src/components/cardDialog.test.tsx
+++ webapp/src/components/cardDialog.test.tsx
@@ -2,6 +2,33 @@
 // See LICENSE.txt for license information.
 
 
+// Polyfill DOMRect for jsdom
+if (typeof globalThis.DOMRect === 'undefined') {
+    globalThis.DOMRect = class DOMRect {
+        x: number
+        y: number
+        width: number
+        height: number
+        top: number
+        right: number
+        bottom: number
+        left: number
+        constructor(x = 0, y = 0, width = 0, height = 0) {
+            this.x = x
+            this.y = y
+            this.width = width
+            this.height = height
+            this.top = y
+            this.right = x + width
+            this.bottom = y + height
+            this.left = x
+        }
+        toJSON() {
+            return {x: this.x, y: this.y, width: this.width, height: this.height, top: this.top, right: this.right, bottom: this.bottom, left: this.left}
+        }
+    } as any
+}
+
 import '@testing-library/jest-dom'
 import {act, render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
